a9bf5c0205f26fad6673dd9df03781c720d03c78
itrump <ye515430@gmai.com>
2022-08-16 08:40:36 +0800

    popup dialog with custom layout

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import android.content.DialogInterface
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -9,9 +10,11 @@ import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.example.myapplication.databinding.ActivityMainBinding
+import com.example.myapplication.databinding.ProgressPopupDialogBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var popUpBinding: ProgressPopupDialogBinding
     private var alertDialog:AlertDialog ? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +29,9 @@ class MainActivity : AppCompatActivity() {
 
         var progressDialog  = binding.progressBarID
         progressDialog.visibility =  View.VISIBLE //View.GONE
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            progressDialog.setProgress(0, true)
+        }
 
         binding.button3.setOnClickListener {
             Log.d("main acitivity", "button 3 clicked")
@@ -34,6 +40,12 @@ class MainActivity : AppCompatActivity() {
 
             // progressBar tutorial refer: https://tutorialwing.com/android-progressbar-using-kotlin-with-example/
             progressDialog.visibility =  if (progressDialog.visibility == View.GONE) View.VISIBLE else View.GONE
+            if (progressDialog.visibility == View.VISIBLE) {
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    progressDialog.setProgress(20, true)
+                }
+            }
 
 
         }
@@ -75,6 +87,24 @@ class MainActivity : AppCompatActivity() {
             alertDialog = alert
         }
 
+        fun withEditText(view: View) {
+            popUpBinding = ProgressPopupDialogBinding.inflate(layoutInflater)
+            var editText = popUpBinding.editText
+            val builder = AlertDialog.Builder(this)
+            val inflater = layoutInflater
+            builder.setTitle("With EditText")
+//            val dialogLayout = inflater.inflate(, null)
+//            val editText  = dialogLayout.findViewById<EditText>(R.id.editText)
+            builder.setView(popUpBinding.root)
+//            builder.setView(dialogLayout)
+            builder.setPositiveButton("OK") { dialogInterface, i -> Toast.makeText(applicationContext, "EditText is " + editText.text.toString(), Toast.LENGTH_SHORT).show() }
+            builder.show()
+        }
+
+        binding.btnAlertWithEditText.setOnClickListener {
+            Toast.makeText(this, "show edit dialog", Toast.LENGTH_SHORT).show()
+            withEditText(binding.root)
+        }
 
     }
 
